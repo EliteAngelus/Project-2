f5ca6ea383c1debc54918b9a1142c27ce2e775f0
Rob <robert.koskulics@me.com>
2018-02-23 09:46:40 -0500

    Modify server file to add chat feature

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var Pusher = require("pusher");
 
 var PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// Set up the pusher information to use chat feature
+var pusher = new Pusher(
+  { appId: "478426",
+    key: "507bfdfaab454a693999",
+    secret: "26efb694b16fb3444fc1",
+    cluster: "us2"
+  });
+
+// Set up var app to use post and get to retrieve chat messages
+app.post('/message', function (req, res) {
+  var message = req.body.message;
+  pusher.trigger('public-chat', 'message-added', { message });
+  res.sendStatus(200);
+});
+app.get('/', function (req, res) {
+  res.sendFile('/public/index.html', { root: __dirname });
+});
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
