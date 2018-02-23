670b8d0c917c272c8651aa23289ab8b2b27175b3
Rob <robert.koskulics@me.com>
2018-02-23 09:54:00 -0500

    Add language.js functions to handle sending and receiving chat messages

--- public/assets/js/language.js
+++ public/assets/js/language.js
@@ -1,1 +1,19 @@
-//HAVENT GOTTEN THIS FAR
+$(document).ready(function () {
+    var pusher = new Pusher('507bfdfaab454a693999', {
+        cluster: 'us2',
+        encrypted: false
+    });
+    let channel = pusher.subscribe('public-language-chat');
+    channel.bind('message-added', onMessageAdded);
+    $('#btn-chat').click(function () {
+        const message = $("#message").val();
+        $("#message").val("");
+        //send message
+        $.post("http://localhost:5000/message", { message });
+    });
+    function onMessageAdded(data) {
+        let template = $("#new-message").html();
+        template = template.replace("{{body}}", data.message);
+        $(".chat").append(template);
+    }
+});
