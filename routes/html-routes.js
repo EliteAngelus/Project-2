d4503edc6fb71173ad6e78ac3021f63c7223c367
1alexamartinez <1alexamartinez@gmail.com>
2018-02-28 19:11:07 -0500

    deleted extra routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,26 +10,13 @@ var path = require("path");
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
 
   // cms route loads cms.html
   app.get("/endChat", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/endChat.html"));
   });
 
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
+ 
 
-  // authors route loads author-manager.html
-  app.get("/authors", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
-  });
-
+ 
 };
