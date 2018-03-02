0fe768010a858f0d527299894aa2edc0adaa932f
EliteAngelus <Angeluselite@gmail.com>
2018-03-02 18:39:27 -0500

    copied data from database2 into master

--- public/assets/js/language.js
+++ public/assets/js/language.js
@@ -30,19 +30,22 @@ $(document).ready(function() {
         const message = $("#message").val();
         $("#message").val("");
         console.log(message)
-            //send message
-
-
-        //*NEW* line 32 allows to log users name in Users table in DB, but wont recognize language or difficulty....and wont show text in chatbox
-        //in order to see what user types in chatbox, change line 32 to $.post("http://localhost:3000/message", { message});
 
         //ALLOWS US TO VIEW OUR TYPED MESSAGE IN THE CHAT BOX
-        // $.post("http://localhost:3000/api/ChatMessages", { message });
         $.post("/message", { message });
         $(".chat").append(name + ": ")
 
     });
+    $('#end-chat').click(function() {
+        //download database as txt file
+        $.post("/endChat", {
 
+
+
+        });
+
+        //clear database
+    });
 
     function onMessageAdded(data) {
         let template = $("#new-message").html();
@@ -54,35 +57,3 @@ $(document).ready(function() {
         $(".chat").append(data.message + "<br>");
     }
 });
-
-// $(document).ready(function() {
-//     $('#chat-page').hide();
-//     $("#get-started").click(function() {
-//         event.preventDefault();
-//         $("#login-form").hide();
-//         $("#chat-page").show();
-//     });
-
-//     var pusher = new Pusher('507bfdfaab454a693999', {
-//         cluster: 'us2',
-//         encrypted: false
-//     });
-//     let channel = pusher.subscribe('public-language-chat');
-//     channel.bind('message-added', onMessageAdded);
-//     $('#btn-chat').click(function() {
-//         const message = $("#message").val();
-//         $("#message").val("");
-//         console.log(message)
-//             //send message
-//         $.post("http://localhost:3000/message", { message });
-//     });
-
-//     function onMessageAdded(data) {
-//         let template = $("#new-message").html();
-//         // template = template.replace("{{body}}", data.message);
-
-//         console.log(template)
-//         console.log(data.message)
-//         $(".chat").append(data.message + "<br>");
-//     }
-// });
